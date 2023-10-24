c553f6d383461bcdd3c1123b1431e09c9c07eda7
Никита Швец <dustikappple@icloud.com>
2023-10-24 20:06:15 +0700

    Initial commit

---
+++ LandingBar/scripts/galleryScripts.js
@@ -0,0 +1,37 @@
+﻿//скрипт для заблюривания фона и добавления линзы
+const modalWindow = document.querySelector(".modal-window");
+const gallery = document.querySelector(".gallery-content");
+
+function blur() {
+  gallery.addEventListener("mouseover", function (event) {
+    const image = event.target;
+    if (image.classList.contains("selected-img")) {
+      image.classList.add("blurred");
+      const markup = `<img src="./assets/galleryPhotos/lens.svg" class="lens"/>`;
+      image.parentNode.insertAdjacentHTML("beforeend", markup);
+    }
+  });
+  gallery.addEventListener("mouseout", function (event) {
+    const image = event.target;
+    if (image.classList.contains("selected-img")) {
+      image.classList.remove("blurred");
+      image.parentNode.querySelector(".lens").remove();
+    }
+  });
+  gallery.addEventListener("click", function (event) {
+    const image = event.target;
+    if (image.classList.contains("selected-img")) {
+      let modalImage = event.target.cloneNode(false);
+      modalImage.removeAttribute("class");
+      modalWindow.classList.add("visible");
+      modalWindow.append(modalImage);
+    }
+  });
+  document.addEventListener("click", (event) => {
+    if (event.target === modalWindow) {
+      modalWindow.classList.remove("visible");
+      modalWindow.innerHTML = ''
+    }
+  });
+}
+blur();
---
+++ LandingBar/scripts/navMenuTransform.js
@@ -0,0 +1,19 @@
+﻿//это пиздец, сюда лучше не смотреть, как сделать не допер, скрипт отключен
+
+var element = document.querySelector(".icon-button");
+
+element.addEventListener("click", function () {
+  var header = document.querySelector("header");
+  var nav = header.querySelector("nav");
+  if (nav.style.display === "none") {
+    nav.style.display = "flex";
+    setTimeout(function () {
+      nav.classList.add("visible");
+    }, 1);
+  } else {
+    nav.classList.remove("visible");
+    setTimeout(function () {
+      nav.style.display = "none";
+    }, 800);
+  }
+});
